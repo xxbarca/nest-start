5ade8cb03de97e4eb2573bb7a082ee1b04ceb30a
liyang <Liyang@18455086673>
2026-02-15 16:42:11 +0800

    设置跨域

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,14 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true, // 允许所有来源
+    // origin: 'http://localhost:3000', // 允许特定来源
+    // origin: ['http://localhost:3000', 'https://example.com'], // 允许多个来源
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    preflightContinue: false,
+  });
   // 全局验证管道
   app.useGlobalPipes(
     new AppValidationPipe({
